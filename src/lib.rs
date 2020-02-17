8d73dcacdd7967d74de38d3052851cdc9d239e06
Willem Melching <willem.melching@gmail.com>
2020-02-16 19:06:18 -0800

    Function names and test cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -19,41 +19,41 @@ pub enum BValue {
     BDict(HashMap<String, BValue>),
 }
 
-fn number(i: &[u8]) -> IResult<&[u8], i64> {
+fn parse_number(i: &[u8]) -> IResult<&[u8], i64> {
     let signed_digit = recognize(pair(opt(char('-')), digit1));
     let parsed_num = map_res(signed_digit, |s: &[u8]| from_utf8(s).unwrap().parse::<i64>());
     terminated(preceded(char('i'), parsed_num), char('e'))(i)
 }
 
-fn length(i: &[u8]) -> IResult<&[u8], usize> {
+fn parse_length(i: &[u8]) -> IResult<&[u8], usize> {
     let len = terminated(digit1, char(':'));
     map_res(len, |s: &[u8]| from_utf8(s).unwrap().parse::<usize>())(i)
 }
 
-fn string(i: &[u8]) -> IResult<&[u8], String> {
-    let (left, len) = length(i)?;
+fn parse_string(i: &[u8]) -> IResult<&[u8], String> {
+    let (left, len) = parse_length(i)?;
     let result = take(len);
     let result = map(result, |s: &[u8]| s.to_vec());
     map_res(result, String::from_utf8)(left)
 }
 
-fn list(i: &[u8]) -> IResult<&[u8], Vec<BValue>> {
-    let values = many1(value);
+fn parse_list(i: &[u8]) -> IResult<&[u8], Vec<BValue>> {
+    let values = many1(parse_value);
     preceded(char('l'), terminated(values, char('e')))(i)
 }
 
-fn dict(i: &[u8]) -> IResult<&[u8], HashMap<String, BValue>> {
-    let kv = pair(string, value);
+fn parse_dict(i: &[u8]) -> IResult<&[u8], HashMap<String, BValue>> {
+    let kv = pair(parse_string, parse_value);
     let kv = many1(kv);
     let kv = terminated(preceded(char('d'), kv), char('e'));
     map(kv, |s| s.into_iter().collect())(i)
 }
 
-fn value(i: &[u8]) -> IResult<&[u8], BValue> {
-    let bnumber = map(number, BValue::BNumber);
-    let bstring = map(string, BValue::BString);
-    let blist = map(list, BValue::BList);
-    let bdict = map(dict, BValue::BDict);
+fn parse_value(i: &[u8]) -> IResult<&[u8], BValue> {
+    let bnumber = map(parse_number, BValue::BNumber);
+    let bstring = map(parse_string, BValue::BString);
+    let blist = map(parse_list, BValue::BList);
+    let bdict = map(parse_dict, BValue::BDict);
     alt((bnumber, bstring, blist, bdict))(i)
 }
 
@@ -61,41 +61,30 @@ fn value(i: &[u8]) -> IResult<&[u8], BValue> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use nom::error::ErrorKind;
-    use nom::Err::Error;
-
 
     #[test]
-    fn parse_number() {
-        assert_eq!(number(&b"i3e"[..]), Ok((&b""[..], 3)));
-        assert_eq!(number(&b"i-3e"[..]), Ok((&b""[..], -3)));
+    fn test_parse_number() {
+        assert_eq!(parse_value(&b"i3e"[..]), Ok((&b""[..], BValue::BNumber(3))));
+        assert_eq!(parse_value(&b"i-3e"[..]), Ok((&b""[..], BValue::BNumber(-3))));
     }
 
     #[test]
-    fn parse_string() {
-        assert_eq!(length(&b"12:Hello World!"[..]), Ok((&b"Hello World!"[..], 12)));
-        assert_eq!(string(&b"12:Hello World!"[..]), Ok((&b""[..], "Hello World!".to_string())));
-        assert_eq!(string(&b"15:Hello World!"[..]), Err(Error((&b"Hello World!"[..], ErrorKind::Eof))));
+    fn test_parse_string() {
+        assert_eq!(parse_value(&b"12:Hello World!"[..]), Ok((&b""[..], BValue::BString("Hello World!".to_string()))));
     }
 
     #[test]
-    fn parse_value() {
-        assert_eq!(value(&b"i3e"[..]), Ok((&b""[..], BValue::BNumber(3))));
-        assert_eq!(value(&b"12:Hello World!"[..]), Ok((&b""[..], BValue::BString("Hello World!".to_string()))));
-    }
-
-    #[test]
-    fn parse_list() {
+    fn test_parse_list() {
         let expected = BValue::BList(vec![BValue::BString("spam".to_string()), BValue::BString("eggs".to_string())]);
-        assert_eq!(value(&b"l4:spam4:eggse"[..]), Ok((&b""[..], expected)));
+        assert_eq!(parse_value(&b"l4:spam4:eggse"[..]), Ok((&b""[..], expected)));
     }
 
     #[test]
-    fn parse_dict() {
+    fn test_parse_dict() {
         let mut expected : HashMap<String, BValue> = HashMap::new();
         expected.entry("cow".to_string()).or_insert(BValue::BString("moo".to_string()));
         expected.entry("spam".to_string()).or_insert(BValue::BString("eggs".to_string()));
 
-        assert_eq!(value(&b"d3:cow3:moo4:spam4:eggse"[..]), Ok((&b""[..], BValue::BDict(expected))));
+        assert_eq!(parse_value(&b"d3:cow3:moo4:spam4:eggse"[..]), Ok((&b""[..], BValue::BDict(expected))));
     }
 }
