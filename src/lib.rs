e57fad54a585f4f772819f1f527c8ba233acf0b3
Willem Melching <willem.melching@gmail.com>
2020-02-16 18:27:33 -0800

    Parse into enum

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,20 @@
 extern crate nom;
 
 use std::str::from_utf8;
+
 use nom::IResult;
 use nom::character::complete::{char, digit1};
 use nom::bytes::complete::take;
 
-use nom::combinator::{map_res, opt, recognize};
+use nom::combinator::{map_res, opt, recognize, map};
 use nom::sequence::{preceded, terminated, tuple};
+use nom::branch::alt;
 
+#[derive(Debug,PartialEq)]
+pub enum BValue<'a> {
+    BString(&'a str),
+    BNumber(i64),
+}
 
 fn number(i: &[u8]) -> IResult<&[u8], i64> {
     let signed_digit = recognize(tuple((opt(char('-')), digit1)));
@@ -24,6 +31,12 @@ fn string(i: &[u8]) -> IResult<&[u8], &str> {
     let (left, len) = string_len(i)?;
     let u8_res = take(len);
     map_res(u8_res, from_utf8)(left)
+}
+
+fn value(i: &[u8]) -> IResult<&[u8], BValue> {
+    let bnumber = map(number, BValue::BNumber);
+    let bstring = map(string, BValue::BString);
+    alt((bnumber, bstring))(i)
 }
 
 
@@ -45,5 +58,11 @@ mod tests {
         assert_eq!(string_len(&b"12:Hello World!"[..]), Ok((&b"Hello World!"[..], 12)));
         assert_eq!(string(&b"12:Hello World!"[..]), Ok((&b""[..], "Hello World!")));
         assert_eq!(string(&b"15:Hello World!"[..]), Err(Error((&b"Hello World!"[..], ErrorKind::Eof))));
+    }
+
+    #[test]
+    fn parse_value() {
+        assert_eq!(value(&b"i3e"[..]), Ok((&b""[..], BValue::BNumber(3))));
+        assert_eq!(value(&b"12:Hello World!"[..]), Ok((&b""[..], BValue::BString("Hello World!"))));
     }
 }
