ac26fb4e3a74705cae9502c30a55683cad87230e
Willem Melching <willem.melching@gmail.com>
2020-02-16 19:03:25 -0800

    Parse dict

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 extern crate nom;
 
 use std::str::from_utf8;
+use std::collections::HashMap;
 
 use nom::IResult;
 use nom::branch::alt;
@@ -8,17 +9,18 @@ use nom::bytes::complete::take;
 use nom::character::complete::{char, digit1};
 use nom::combinator::{map_res, opt, recognize, map};
 use nom::multi::many1;
-use nom::sequence::{preceded, terminated, tuple};
+use nom::sequence::{preceded, terminated, pair};
 
 #[derive(Debug,PartialEq)]
 pub enum BValue {
     BString(String),
     BNumber(i64),
     BList(Vec<BValue>),
+    BDict(HashMap<String, BValue>),
 }
 
 fn number(i: &[u8]) -> IResult<&[u8], i64> {
-    let signed_digit = recognize(tuple((opt(char('-')), digit1)));
+    let signed_digit = recognize(pair(opt(char('-')), digit1));
     let parsed_num = map_res(signed_digit, |s: &[u8]| from_utf8(s).unwrap().parse::<i64>());
     terminated(preceded(char('i'), parsed_num), char('e'))(i)
 }
@@ -40,11 +42,19 @@ fn list(i: &[u8]) -> IResult<&[u8], Vec<BValue>> {
     preceded(char('l'), terminated(values, char('e')))(i)
 }
 
+fn dict(i: &[u8]) -> IResult<&[u8], HashMap<String, BValue>> {
+    let kv = pair(string, value);
+    let kv = many1(kv);
+    let kv = terminated(preceded(char('d'), kv), char('e'));
+    map(kv, |s| s.into_iter().collect())(i)
+}
+
 fn value(i: &[u8]) -> IResult<&[u8], BValue> {
     let bnumber = map(number, BValue::BNumber);
     let bstring = map(string, BValue::BString);
     let blist = map(list, BValue::BList);
-    alt((bnumber, bstring, blist))(i)
+    let bdict = map(dict, BValue::BDict);
+    alt((bnumber, bstring, blist, bdict))(i)
 }
 
 
@@ -76,7 +86,16 @@ mod tests {
 
     #[test]
     fn parse_list() {
-        let result = BValue::BList(vec![BValue::BString("spam".to_string()), BValue::BString("eggs".to_string())]);
-        assert_eq!(value(&b"l4:spam4:eggse"[..]), Ok((&b""[..], result)));
+        let expected = BValue::BList(vec![BValue::BString("spam".to_string()), BValue::BString("eggs".to_string())]);
+        assert_eq!(value(&b"l4:spam4:eggse"[..]), Ok((&b""[..], expected)));
+    }
+
+    #[test]
+    fn parse_dict() {
+        let mut expected : HashMap<String, BValue> = HashMap::new();
+        expected.entry("cow".to_string()).or_insert(BValue::BString("moo".to_string()));
+        expected.entry("spam".to_string()).or_insert(BValue::BString("eggs".to_string()));
+
+        assert_eq!(value(&b"d3:cow3:moo4:spam4:eggse"[..]), Ok((&b""[..], BValue::BDict(expected))));
     }
 }
