b10e41f4849b71052ec1d78a96b171bcdb534d8c
Willem Melching <willem.melching@gmail.com>
2020-02-16 18:30:45 -0800

    Put String in enum

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,8 @@ use nom::sequence::{preceded, terminated, tuple};
 use nom::branch::alt;
 
 #[derive(Debug,PartialEq)]
-pub enum BValue<'a> {
-    BString(&'a str),
+pub enum BValue {
+    BString(String),
     BNumber(i64),
 }
 
@@ -27,10 +27,11 @@ fn string_len(i: &[u8]) -> IResult<&[u8], usize> {
     map_res(len, |s: &[u8]| from_utf8(s).unwrap().parse::<usize>())(i)
 }
 
-fn string(i: &[u8]) -> IResult<&[u8], &str> {
+fn string(i: &[u8]) -> IResult<&[u8], String> {
     let (left, len) = string_len(i)?;
     let u8_res = take(len);
-    map_res(u8_res, from_utf8)(left)
+    let vec_res = map(u8_res, |s: &[u8]| s.to_vec());
+    map_res(vec_res, String::from_utf8)(left)
 }
 
 fn value(i: &[u8]) -> IResult<&[u8], BValue> {
@@ -56,13 +57,13 @@ mod tests {
     #[test]
     fn parse_string() {
         assert_eq!(string_len(&b"12:Hello World!"[..]), Ok((&b"Hello World!"[..], 12)));
-        assert_eq!(string(&b"12:Hello World!"[..]), Ok((&b""[..], "Hello World!")));
+        assert_eq!(string(&b"12:Hello World!"[..]), Ok((&b""[..], "Hello World!".to_string())));
         assert_eq!(string(&b"15:Hello World!"[..]), Err(Error((&b"Hello World!"[..], ErrorKind::Eof))));
     }
 
     #[test]
     fn parse_value() {
         assert_eq!(value(&b"i3e"[..]), Ok((&b""[..], BValue::BNumber(3))));
-        assert_eq!(value(&b"12:Hello World!"[..]), Ok((&b""[..], BValue::BString("Hello World!"))));
+        assert_eq!(value(&b"12:Hello World!"[..]), Ok((&b""[..], BValue::BString("Hello World!".to_string()))));
     }
 }
